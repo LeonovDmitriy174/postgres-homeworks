cc97ab9b3f72c99cc22d4107d04d6f7f424e829e
Дмитрий Леонов <dmitriy.leonov.1993@mail.ru>
2024-03-13 02:26:59 +0500

    made first homework

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) not NULL,
+	last_name varchar(100) not NULL,
+	title varchar(100) not NULL,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(100) not NULL,
+	contact_name varchar(100) not NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	employee_id int REFERENCES employees(employee_id) not NULL,
+	customer_id varchar(10) REFERENCES customers(customer_id) not NULL,
+	order_date date not NULL,
+	ship_city varchar(100) not NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,54 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+import csv
+
+password = os.environ.get('MY_PASSWORD')
+
+
+def open_file(file_name):
+    with open(os.path.join("north_data", file_name), newline='', encoding='windows-1251') as csvfile:
+        file = csv.DictReader(csvfile)
+        new_list = []
+        for i in file:
+            new_list.append(i)
+        return new_list
+
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password=password
+)
+try:
+    with conn.cursor() as cur:
+        rows = open_file("employees_data.csv")
+        for row in rows:
+            cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", (row['employee_id'],
+                                                                                  row['first_name'],
+                                                                                  row['last_name'],
+                                                                                  row['title'],
+                                                                                  row['birth_date'],
+                                                                                  row['notes']))
+        conn.commit()
+
+    with conn.cursor() as cur_two:
+        rows = open_file("customers_data.csv")
+        for row in rows:
+            cur_two.execute("INSERT INTO customers VALUES (%s, %s, %s)", (row['customer_id'],
+                                                                          row['company_name'],
+                                                                          row['contact_name']))
+        conn.commit()
+
+    with conn.cursor() as cur_three:
+        rows = open_file("orders_data.csv")
+        for row in rows:
+            cur_three.execute(f"INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", (row['order_id'],
+                                                                                  row['employee_id'],
+                                                                                  row['customer_id'],
+                                                                                  row['order_date'],
+                                                                                  row['ship_city']))
+        conn.commit()
+finally:
+    conn.close()
